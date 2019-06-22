bc8bc2fff6b745f2e73843e3e64f68878cbdaada
jdavidg1 <jdavidg_1@hotmail.com>
2019-06-22 18:22:40 -0500

    Funcionando V1.0

--- EjercicioFor/Program.cs
+++ EjercicioFor/Program.cs
@@ -10,6 +10,39 @@ namespace EjercicioFor
     {
         static void Main(string[] args)
         {
+            //Definición de variables
+
+            int n = 0; // control del ciclo
+            int cantidad = 0; //Cantidad de alumnos
+            float calif = 0.0f, suma = 0.0f, promedio = 0.0f;
+
+
+            // Comienzo del programa e inicialización de variables por el usuario
+
+            Console.WriteLine("Cual es la cantidad de alumnos: ");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+
+            //Comienzo de la lectura de datos
+
+            for(n=1; n<=cantidad; n++)
+            {
+                Console.WriteLine("Dame la calificación del alumno: ");
+
+                calif = Convert.ToSingle(Console.ReadLine());
+                //llevamos a cabo la suma de las calificaciones
+
+                suma += calif;
+                
+
+            }
+
+            //Calcular el promedio
+
+            promedio = suma / cantidad;
+
+            Console.WriteLine($"El promedio es {promedio} de los {cantidad} alumnos");
+            Console.ReadKey();
+
         }
     }
 }
